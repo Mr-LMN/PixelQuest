6d0d1d90eb9b1b53891adb8d538c869f9dd23527
Mr-LMN <lloydmartin94@gmail.com>
2026-03-11 05:34:36 -0700

    Correct HubScene preload: place spritesheet call inside function

--- src/game/scenes/HubScene.js
+++ src/game/scenes/HubScene.js
@@ -29,8 +29,9 @@ export class HubScene extends Phaser.Scene {
   preload() {
     this.load.image(HUB_TILESET_KEY, HUB_TILESET_PATH);
     this.load.tilemapTiledJSON(HUB_MAP_KEY, HUB_MAP_PATH);
+        this.load.spritesheet('player', 'assets/characters/player/fitness_teacher_sprite_sheet.png', { frameWidth: 48, frameHeight: 48 });
+    
   }
-      this.load.spritesheet('player', 'assets/characters/player/fitness_teacher_sprite_sheet.png', { frameWidth: 48, frameHeight: 48 });
 
   init(data) {
   }
